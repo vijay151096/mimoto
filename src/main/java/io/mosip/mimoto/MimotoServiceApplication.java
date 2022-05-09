ed3ef5d45cbc6b779d51ed64797ff8bc4f002472
Ken Lewerentz <kneckinator@gmail.com>
2022-05-09 13:49:22 +0700

    Revert "Version 1.2.0 - latest develop"

--- src/main/java/io/mosip/mimoto/MimotoServiceApplication.java
+++ src/main/java/io/mosip/mimoto/MimotoServiceApplication.java
@@ -1,8 +1,5 @@
 package io.mosip.mimoto;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.json.simple.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
@@ -18,18 +15,22 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import io.mosip.mimoto.service.impl.CbeffImpl;
 import io.mosip.mimoto.spi.CbeffUtil;
 
-@SpringBootApplication(scanBasePackages = {
+@SpringBootApplication(
+    scanBasePackages = {
         "io.mosip.mimoto.*",
         "${mosip.auth.adapter.impl.basepackage}"
-}, exclude = {
+    },
+    exclude = {
         SecurityAutoConfiguration.class,
         DataSourceAutoConfiguration.class,
         HibernateJpaAutoConfiguration.class,
         CacheAutoConfiguration.class
-})
+    }
+)
 @EnableScheduling
 @EnableAsync
 public class MimotoServiceApplication {
+
     @Bean
     @Primary
     public CbeffUtil getCbeffUtil() {
@@ -44,27 +45,7 @@ public class MimotoServiceApplication {
         return threadPoolTaskScheduler;
     }
 
-    public static JSONObject getGitProp() {
-        try {
-            return (new ObjectMapper()).readValue(
-                MimotoServiceApplication.class.getClassLoader().getResourceAsStream("build.json"),
-                JSONObject.class
-            );
-        } catch (Exception e) {
-            System.err.println("Error when trying to read build.json file: " + e);
-        }
-        return new JSONObject();
-    }
-
     public static void main(String[] args) {
-        JSONObject gitProp = getGitProp();
-        System.out.println(
-                String.format(
-                        "Mimoto Service version: %s - revision: %s @ branch: %s | build @ %s",
-                        gitProp.get("git.build.version"),
-                        gitProp.get("git.commit.id.abbrev"),
-                        gitProp.get("git.branch"),
-                        gitProp.get("git.build.time")));
         SpringApplication.run(MimotoServiceApplication.class, args);
     }
 
