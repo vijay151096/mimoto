9132d2fe68435f3ceb855c54ce8e93e1f5dc10fc
Monobikash Das <monobikash.das@mindtree.com>
2022-12-10 13:29:41 +0530

    Added link-transaction mock code

---
+++ src/main/java/io/mosip/mimoto/dto/mimoto/LinkCodeResponse.java
@@ -0,0 +1,18 @@
+package io.mosip.mimoto.dto.mimoto;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class LinkCodeResponse {
+    private String linkTransactionId;
+    private String clientName;
+    private String logoUrl;
+    private List<AuthFactorDto> authFactors;
+    private List<String> authorizeScopes;
+    private List<String> essentialClaims;
+    private List<String> voluntaryClaims;
+    private Map<String, Object> configs;
+}
