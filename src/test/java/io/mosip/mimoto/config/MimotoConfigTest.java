bed86257ff613f69e6dd204c233f1238bdcb3bd7
Kiruthika Jeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
2023-08-04 13:24:08 +0530

    refactor(INJI-219): [Kiruthika] revert add test for MimotoConfigTest

--- src/test/java/io/mosip/mimoto/config/MimotoConfigTest.java
+++ src/test/java/io/mosip/mimoto/config/MimotoConfigTest.java
@@ -1,15 +1,15 @@
 package io.mosip.mimoto.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
+import io.mosip.kernel.auth.defaultadapter.filter.AuthFilter;
+import io.mosip.kernel.auth.defaultadapter.filter.CorsFilter;
+import io.mosip.mimoto.config.Config;
+import io.mosip.mimoto.config.OpenApiProperties;
+import io.mosip.mimoto.config.SwaggerConfig;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.TrustStrategy;
-import org.junit.Before;
-import org.junit.Test;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +17,6 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.SSLContext;
@@ -26,20 +25,13 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import static org.junit.Assert.assertEquals;
-
 
 @Configuration
 @ComponentScan(basePackages = {
-        "io.mosip.mimoto.*"}, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = {
+        "io.mosip.mimoto.*" }, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = {
         Config.class, OpenApiProperties.class, SwaggerConfig.class}))
 public class MimotoConfigTest extends WebSecurityConfigurerAdapter {
-    OpenApiProperties openApiProperties = new OpenApiProperties();
-
-    SwaggerConfig swaggerConfig = new SwaggerConfig();
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -92,43 +84,5 @@ public class MimotoConfigTest extends WebSecurityConfigurerAdapter {
     @Bean
     public Map<String, String> injiConfig() {
         return new HashMap<>();
-    }
-
-    @Before
-    public void setUp() {
-        InfoProperty info = new InfoProperty();
-        info.setTitle("infoTitle");
-        info.setVersion("infoVersion");
-        info.setDescription("infoDescription");
-        LicenseProperty license = new LicenseProperty();
-        license.setName("licenseName");
-        license.setUrl("licenseUrl");
-        info.setLicense(license);
-        Service service = new Service();
-        Server server = new Server();
-        server.setDescription("serverDescription");
-        server.setUrl("serverUrl");
-        service.setServers(List.of(server));
-        openApiProperties.setService(service);
-        openApiProperties.setInfo(info);
-        ReflectionTestUtils.setField(swaggerConfig, "openApiProperties", openApiProperties);
-    }
-
-    @Test
-    public void testSwaggerConfigOpenApi() {
-        OpenAPI expectedOpenAPI = new OpenAPI()
-                .components(new Components())
-                .info(new Info()
-                        .title("infoTitle")
-                        .version("infoVersion")
-                        .description("infoDescription")
-                        .license(new License()
-                                .name("licenseName")
-                                .url("licenseUrl")));
-        expectedOpenAPI.addServersItem(new io.swagger.v3.oas.models.servers.Server().description("serverDescription").url("serverUrl"));
-
-        OpenAPI actualOpenApi = swaggerConfig.openApi();
-
-        assertEquals(expectedOpenAPI, actualOpenApi);
     }
 }
